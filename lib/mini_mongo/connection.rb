1ffca47967314989893fc94fea472b10634a8ece
robertomiranda <rjmaltamar@gmail.com>
2012-07-27 14:57:03 -0500

    add slave_ok setter

--- lib/mini_mongo/connection.rb
+++ lib/mini_mongo/connection.rb
@@ -2,6 +2,12 @@ require "uri"
 require "mongo"
 module MiniMongo
   module Connection
+    @@slave_ok = false
+
+    def slave_ok=(attr)
+      @@slave_ok = attr
+    end
+
     def database_url=(url)
       @@database_url = url
     end
@@ -9,7 +15,7 @@ module MiniMongo
     def db_connection
       db = URI.parse(@@database_url)
       db_name = db.path.gsub(/^\//, '')
-      @@db_connection ||= Mongo::Connection.new(db.host, db.port, :slave_ok => true).db(db_name)
+      @@db_connection ||= Mongo::Connection.new(db.host, db.port, :slave_ok => @@slave_ok).db(db_name)
       @@db_connection.authenticate(db.user, db.password) if db.user && !db.user.empty?
       @@db_connection
     end
